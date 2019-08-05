45f98693a8ed469ee20546c683f7b4f2fe0f6c67
sak1b1 <abdur.rahman@nascenia.com>
2019-08-05 12:50:33 +0600

    before adding the cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  #get 'store/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'welcome#index'
+  root to: 'store#index', as: 'store_index'
   resources :products
 end
