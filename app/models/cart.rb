eeacd80c8e17df74622923b22d14a23a47559176
sak1b1 <abdur.rahman@nascenia.com>
2019-08-07 10:57:35 +0600

    ajax added but doesn't dynamically show yet

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,14 +3,19 @@ class Cart < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
   def add_product(product)
-    current_item= line_items.find_by(product_id: product.id)
+    current_item = line_items.find_by(product_id: product.id)
     if current_item
-      current_item +=1
+      current_item.quantity += 1
     else
       current_item= line_items.build(product_id: product.id)
     end
     current_item
   end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
   private
     def ensure_not_referenced_by_any_line_item
       unless line_items.empty?
